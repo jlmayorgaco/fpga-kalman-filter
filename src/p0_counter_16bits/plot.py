ef5f890bc08877a1fc03ffe6654261b44bd5d772
Ing. Jorge Luis Mayorga Taboda <167396627+jlmayorgaco@users.noreply.github.com>
2024-06-25 14:24:11 -0500

    Update plot.py

--- src/p0_counter_16bits/plot.py
+++ src/p0_counter_16bits/plot.py
@@ -1,23 +1,21 @@
-from p0_counter_16bits.scripts.utils.VCDSignalsClass import VCDSignalsClass
-from p0_counter_16bits.scripts.graphs.GraphChartClass import GraphChartClass
-from p0_counter_16bits.scripts.graphs.GraphBitStreamClass import GraphBitStreamClass
-from p0_counter_16bits.scripts.boards.ExpResultGraphBoardClass import ExpResultGraphBoardClass
+from scripts.utils.VCDSignalsClass import VCDSignalsClass
+#from scripts.graphs.GraphChartClass import GraphChartClass
+#from scripts.graphs.GraphBitStreamClass import GraphBitStreamClass
+#from scripts.boards.ExpResultGraphBoardClass import ExpResultGraphBoardClass
 
 if __name__ == "__main__":
 
     vcd = VCDSignalsClass()
     vcd.setFileVCD('p0_counter_16bits_wave.vcd')
     vcd.setFileJSON('output.json')
-    vcd.addSignalByName('')
-    vcd.addSignalByName('')
-    vcd.addSignalByName('')
-
-    signals = vcd.getSignals()
-    signal_clk = signals.getByName('')
-    signal_rst = signals.getByName('')
-    signal_counter = signals.getByName('')
+    vcd.setModuleName('up_counter_16bit_tb')
+    vcd.start()
 
     '''
+    signal_clk = vcd.getSignalByName('clk')
+    signal_rst = vcd.getSignalByName('reset')
+    signal_counter = vcd.getSignalByName('count[15:0]')
+
     chartGraph = GraphChartClass()
     chartGraph.addSignal(signal_counter)
 
