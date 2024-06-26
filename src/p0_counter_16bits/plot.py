abac6e4c2134e20a3c4ac1c6cc357496c8e5a893
Ing. Jorge Luis Mayorga Taboda <167396627+jlmayorgaco@users.noreply.github.com>
2024-06-25 21:19:29 -0500

    Update plot.py

--- src/p0_counter_16bits/plot.py
+++ src/p0_counter_16bits/plot.py
@@ -1,28 +1,57 @@
 from scripts.utils.VCDSignalsClass import VCDSignalsClass
-#from scripts.graphs.GraphChartClass import GraphChartClass
-#from scripts.graphs.GraphBitStreamClass import GraphBitStreamClass
+from scripts.graphs.GraphChartClass import GraphChartClass
+from scripts.graphs.GraphBitStreamClass import GraphBitStreamClass
 #from scripts.boards.ExpResultGraphBoardClass import ExpResultGraphBoardClass
 
 if __name__ == "__main__":
 
+    # ----------------------------------------------------------- #
+    # --  VCD File                                            --- #
+    # ----------------------------------------------------------- #
     vcd = VCDSignalsClass()
     vcd.setFileVCD('p0_counter_16bits_wave.vcd')
     vcd.setFileJSON('output.json')
     vcd.setModuleName('up_counter_16bit_tb')
     vcd.start()
+    # ----------------------------------------------------------- #
 
-    '''
+
+    # ----------------------------------------------------------- #
+    # --  Signals                                             --- #
+    # ----------------------------------------------------------- #
     signal_clk = vcd.getSignalByName('clk')
-    signal_rst = vcd.getSignalByName('reset')
-    signal_counter = vcd.getSignalByName('count[15:0]')
+    signal_clk.setType('bit')
 
+    signal_rst = vcd.getSignalByName('reset')
+    signal_rst.setType('bit')
+
+    signal_counter = vcd.getSignalByName('count[15:0]')
+    signal_counter.setType('int')
+    # ----------------------------------------------------------- #
+
+
+    # ----------------------------------------------------------- #
+    # --  Charts                                              --- #
+    # ----------------------------------------------------------- #
     chartGraph = GraphChartClass()
+    chartGraph.addTitle('Counter 16 Bits')
+    chartGraph.addLabelX('Time [ms]')
+    chartGraph.addLabelY('Value')
     chartGraph.addSignal(signal_counter)
 
     bitstreamGraph = GraphBitStreamClass()
-    bitstreamGraph.addBitSignal(signal_clk)
-    bitstreamGraph.addBitSignal(signal_rst)
-    bitstreamGraph.addHextSignal(signal_counter)
+    bitstreamGraph.addSignal(signal_clk)
+    bitstreamGraph.addSignal(signal_rst)
+    bitstreamGraph.addSignal(signal_counter)
+    # ----------------------------------------------------------- #
+
+    print('bitstreamGraph')
+    print(bitstreamGraph)
+    print('0o9')
+
+    '''
+
+
 
     board = ExpResultGraphBoardClass()
     board.setBitStreamGraph(bitstreamGraph)
